3f08c6ac241efadfbb3ac6f52c65f78c8e3243cf
vesingar <vesingar@cisco.com>
2024-08-11 17:40:44 +0530

    Portfolio Website

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const buttons = document.querySelectorAll('.download-resume-btn');
+  buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      const link = document.createElement('a');
+      link.href = 'assets/Resume/Resume.pdf';
+      link.download = 'Resume.pdf'; 
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    });
+  });
+});
